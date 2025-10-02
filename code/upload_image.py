c0ec8f524af98bf96b9a77abe419fa082d5fe5a0
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-10-01 20:36:31 -0700

    add image

--- code/upload_image.py
+++ code/upload_image.py
@@ -4,5 +4,5 @@ from PIL import Image, ImageDraw, ImageFont
 epd = epd2in13d.EPD()
 epd.init()
 epd.Clear()
-image = Image.open("dopet.bmp")
+image = Image.open("dopet.png")
 epd.display(epd.getbuffer(image))
